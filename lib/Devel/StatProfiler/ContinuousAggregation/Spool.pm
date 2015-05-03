10d1ee67e69ab9270f4dcb939338a5faf7b38fe0
Mattia Barbon <mattia@barbon.org>
2015-05-03 18:08:22 +0200

    Allow specifying an explict file target name in to_spool
    For the cases when the source file is a temporary file (e.g. file uploads).

--- lib/Devel/StatProfiler/ContinuousAggregation/Spool.pm
+++ lib/Devel/StatProfiler/ContinuousAggregation/Spool.pm
@@ -12,7 +12,9 @@ use Devel::StatProfiler::Reader;
 
 sub to_spool {
     my (%args) = @_;
-    my $file = $args{file} // die "File is mandatory";
+    my $file_target = $args{file} // die "File is mandatory";
+    my $file = ref($file_target) ? $file_target->[0] : $file_target;
+    my $target = ref($file_target) ? $file_target->[1] : File::Basename::basename($file);
     my $root_directory = $args{root_directory} // die "Root directory is mandatory";
     my $kind = $args{kind} // 'default';
     my $reader = Devel::StatProfiler::Reader->new($file);
@@ -20,7 +22,7 @@ sub to_spool {
     my $aggregation_id = $metadata->{aggregation_id} // 'default';
 
     my $dest_dir = $root_directory . '/spool/' . $kind . '/' . $aggregation_id;
-    my $dest = $dest_dir . '/' . File::Basename::basename($file);
+    my $dest = $dest_dir . '/' . $target;
     my $tmp = $dest . '.tmp';
 
     File::Path::mkpath([$dest_dir]);
